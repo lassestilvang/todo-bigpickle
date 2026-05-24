f0b188b3501f56c617bd89b8cd802c99d5c182a8
Lasse Stilvang <lasse.stilvang@gmail.com>
2026-05-24 23:23:00 +0200

    test: update jest-dom type declarations for bun

--- src/types/jest-dom.d.ts
+++ src/types/jest-dom.d.ts
@@ -1,11 +1,1 @@
 import '@testing-library/jest-dom'
-
-declare global {
-  namespace jest {
-    interface Matchers<R> {
-      toBeInTheDocument(): R
-      toBeChecked(): R
-      toHaveClass(className: string): R
-    }
-  }
-}
