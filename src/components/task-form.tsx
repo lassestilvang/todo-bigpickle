bcb20f5833e8be2d6458f5f1b1614a7e8d4a81c0
Lasse Stilvang <lasse.stilvang@gmail.com>
2026-06-11 23:23:00 +0200

    fix: add missing useCallback import in task-form

--- src/components/task-form.tsx
+++ src/components/task-form.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useRef, useEffect, memo } from 'react'
+import { useState, useRef, useEffect, useCallback, memo } from 'react'
 import { useForm, useWatch } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import * as z from 'zod'
